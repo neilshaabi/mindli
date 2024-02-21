352d023d8bc8561023af3d8f2b3cb9364c270920
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2024-02-21 21:11:51 +0000

    Updated mail template

--- app/utils/mail.py
+++ app/utils/mail.py
@@ -10,8 +10,8 @@ from app.models import User
 
 @unique
 class EmailSubject(Enum):
-    EMAIL_VERIFICATION = "Email Verification"
-    PASSWORD_RESET = "Password Reset"
+    EMAIL_VERIFICATION = "Email verification"
+    PASSWORD_RESET = "Password reset"
 
 
 class EmailMessage:
