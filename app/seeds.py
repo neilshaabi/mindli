f60fc36a82355ea79b2feef57908414523f32183
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2024-02-27 23:15:51 +0000

    Seed languages

---
+++ app/seeds.py
@@ -0,0 +1,37 @@
+import click
+import pycountry
+from flask import Flask
+
+from app import db
+from app.models.language import Language
+
+
+def register_cli_commands(app: Flask):
+    @app.cli.command("seed-db")
+    def seed_db() -> None:
+        seed_languages()
+        click.echo("Seeding database with languages...")
+        return
+
+    return
+
+
+def seed_languages() -> None:
+    languages = []
+    for language in pycountry.languages:
+        if language.type == "L":  # Only include 'living' languages
+            languages.append(
+                Language(
+                    name=language.name,
+                    alpha_2=getattr(language, "alpha_2", None),
+                    alpha_3=getattr(language, "alpha_3", None),
+                )
+            )
+    db.session.add_all(languages)
+    db.session.commit()
+
+    count = db.session.execute(
+        db.select(db.func.count()).select_from(Language)
+    ).scalar()
+    print(count)
+    return
