47964b92b014d6d49de38195b38cb9fde93fc6cb
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2024-02-27 18:08:21 +0000

    Add full stops

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -32,16 +32,16 @@ class PasswordValidator:
         error: str = None
 
         if len(password) < 8:
-            error = "Password must be at least 8 characters long"
+            error = "Password must be at least 8 characters long."
 
         elif not any(char.isdigit() for char in password):
-            error = "Password must include at least one digit"
+            error = "Password must include at least one digit."
 
         elif not any(char.isupper() for char in password):
-            error = "Password must include at least one uppercase letter"
+            error = "Password must include at least one uppercase letter."
 
         elif not any(char.islower() for char in password):
-            error = "Password must include at least one lowercase letter"
+            error = "Password must include at least one lowercase letter."
 
         if error:
             raise ValidationError(error)
